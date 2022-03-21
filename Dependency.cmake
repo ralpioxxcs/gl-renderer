3d05af2d0158d8b77612e49ecb4200a66880ce21
ralpioxxcs <tmfmfkal1123@naver.com>
2022-03-21 23:40:16 +0900

    initalize & window

--- Dependency.cmake
+++ Dependency.cmake
@@ -7,14 +7,15 @@
 
 include(ExternalProject)
 
-set(DEP_INSTALL_DIR ${PROJECT_THIRED_PARTY_DIR}/install)
+set(DEP_THIRED_PARTY_DIT ${PROJECT_BINARY_DIR}/third-party)
+set(DEP_INSTALL_DIR ${PROJECT_BINARY_DIR}/install)
 set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
 set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
 
 # glfw
 ExternalProject_Add(
     dep_glfw
-    PREFIX ${PROJECT_THIRED_PARTY_DIR}
+    PREFIX ${PROJECT_BINARY_DIR}
     GIT_REPOSITORY "https://github.com/glfw/glfw.git"
     CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
@@ -28,11 +29,15 @@ set(DEP_LIBS ${DEP_LIBS} glfw3)
 # glad
 ExternalProject_Add(
     dep_glad
-    PREFIX ${PROJECT_THIRED_PARTY_DIR}
     GIT_REPOSITORY "https://github.com/Dav1dde/glad"
+    GIT_TAG "v0.1.34"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
     CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
         -DGLAD_INSTALL=ON
+    PREFIX ${PROJECT_BINARY_DIR}
 )
 set(DEP_LIST ${DEP_LIST} dep_glad)
 set(DEP_LIBS ${DEP_LIBS} glad)
@@ -40,45 +45,35 @@ set(DEP_LIBS ${DEP_LIBS} glad)
 # glm
 ExternalProject_Add(
     dep_glm
-    PREFIX ${PROJECT_THIRED_PARTY_DIR}
+    PREFIX ${PROJECT_BINARY_DIR}
     GIT_REPOSITORY "https://github.com/g-truc/glm"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     TEST_COMMAND ""
     INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory
-        ${PROJECT_THIRED_PARTY_DIR}/src/dep_glm/glm
+        ${PROJECT_BINARY_DIR}/src/dep_glm/glm
         ${DEP_INSTALL_DIR}/include/glm
 )
 set(DEP_LIST ${DEP_LIST} dep_glm)
-set(DEP_LIBS ${DEP_LIBS} glm)
     
 # imgui
 ExternalProject_Add(
     dep_imgui
-    PREFIX ${PROJECT_THIRED_PARTY_DIR}
+    PREFIX ${PROJECT_BINARY_DIR}
     URL "https://github.com/ocornut/imgui/archive/refs/tags/v1.84.2.zip"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
     COMMAND ${CMAKE_COMMAND} -E copy
-        ${PROJECT_THIRED_PARTY_DIR}/src/dep_imgui/backends/imgui_impl_glfw.cpp
-        ${PROJECT_THIRED_PARTY_DIR}/src/dep_imgui
+        ${PROJECT_BINARY_DIR}/src/dep_imgui/backends/imgui_impl_glfw.cpp
+        ${PROJECT_BINARY_DIR}/src/dep_imgui
     COMMAND ${CMAKE_COMMAND} -E copy
-        ${PROJECT_THIRED_PARTY_DIR}/src/dep_imgui/backends/imgui_impl_glfw.h
-        ${PROJECT_THIRED_PARTY_DIR}/src/dep_imgui
+        ${PROJECT_BINARY_DIR}/src/dep_imgui/backends/imgui_impl_glfw.h
+        ${PROJECT_BINARY_DIR}/src/dep_imgui
     COMMAND ${CMAKE_COMMAND} -E copy
-        ${PROJECT_THIRED_PARTY_DIR}/src/dep_imgui/backends/imgui_impl_opengl3.cpp
-        ${PROJECT_THIRED_PARTY_DIR}/src/dep_imgui
+        ${PROJECT_BINARY_DIR}/src/dep_imgui/backends/imgui_impl_opengl3.cpp
+        ${PROJECT_BINARY_DIR}/src/dep_imgui
     COMMAND ${CMAKE_COMMAND} -E copy
-        ${PROJECT_THIRED_PARTY_DIR}/src/dep_imgui/backends/imgui_impl_opengl3.h
-        ${PROJECT_THIRED_PARTY_DIR}/src/dep_imgui
+        ${PROJECT_BINARY_DIR}/src/dep_imgui/backends/imgui_impl_opengl3.h
+        ${PROJECT_BINARY_DIR}/src/dep_imgui
 )
-# add_library(imgui "")
-# file(GLOB imgui_sources ${PROJECT_THIRED_PARTY_DIR}/src/dep_imgui/*.cpp)
-# target_include_directories(imgui PRIVATE ${DEP_INCLUDE_DIR})
-# target_sources(imgui PRIVATE ${imgui_sources})
-# add_dependencies(imgui ${DEP_LIST})
-
-# set(DEP_INCLUDE_DIR ${DEP_INCLUDE_DIR} ${PROJECT_THIRED_PARTY_DIR}/src/dep_imgui)
-# set(DEP_LIST ${DEP_LIST} imgui)
-# set(DEP_LIBS ${DEP_LIBS} imgui)
---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+static void OnErrorCallback(int errorCode, const char* errorDescription) {
+  std::cerr << "Error : " << errorDescription << std::endl;
+}
+
+void OnKeyCallback(GLFWwindow* wnd, int key, int scancode, int action, int
+mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(wnd, GLFW_TRUE);
+  }
+}
+
+void OnFramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+int main(int argc, char* argv[]) {
+  // glfwSetErrorCallback(errorCallback);
+
+  if (!glfwInit()) {
+    const char* desc = nullptr;
+    glfwGetError(&desc);
+    std::cerr << "Error: failed to initialize GLFW (" << desc << ")" << std::endl;
+    return -1;
+  }
+
+  const int wndWidth = 800;
+  const int wndHeight = 600;
+  const char* wndName = "window";
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // core profile (not compatible)
+
+  GLFWwindow* wnd = glfwCreateWindow(wndWidth, wndHeight, wndName, nullptr, nullptr);
+  if (!wnd) {
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(wnd);  // GLFW가 현재 window을 main context로 만들도록 함
+
+  OnFramebufferSizeCallback(wnd, wndWidth, wndHeight);
+  glfwSetFramebufferSizeCallback(wnd, OnFramebufferSizeCallback);
+  glfwSetKeyCallback(wnd, OnKeyCallback);
+
+  std::cout << "OpenGL version : " << glGetString(GL_VERSION) << std::endl;
+  std::cout << "GLSL version : " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+  std::cout << "Vendor : " << glGetString(GL_VENDOR) << std::endl;
+  std::cout << "Renderer : " << glGetString(GL_RENDER) << std::endl;
+
+  while (!glfwWindowShouldClose(wnd)) {
+    glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(wnd);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return 0;
+}
